a804a753b68084b6cebf52ff8249928849ed764e
yash sahu <sahuyash1103@gmail.com>
2023-12-03 20:52:32 +0530

    access conrtoll origin fixing

--- api/src/index.js
+++ api/src/index.js
@@ -12,9 +12,16 @@ const corsOptions = {
     origin: ["http://localhost:3001", process.env.CLIENT_URL],
 }
 
-console.log(process.env.CLIENT_URL)
-
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', '*');
+    console.log(req.headers.origin);
+    next();
+})
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(expressSession(
@@ -32,7 +39,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.json(['Hello World', process.env.CLIENT_URL]);
 });
 
 app.get('/login', (req, res) => {
