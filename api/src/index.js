ca37328de10967b7993613e0c97358db593500d2
yash sahu <sahuyash1103@gmail.com>
2023-12-03 20:55:15 +0530

    testing cors

--- api/src/index.js
+++ api/src/index.js
@@ -15,9 +15,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', '*');
+    // res.header('Access-Control-Allow-Credentials', true);
+    // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    // res.header('Access-Control-Allow-Headers', '*');
     console.log(req.headers.origin);
     next();
 })
